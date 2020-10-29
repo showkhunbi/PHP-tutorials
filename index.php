8a9b7fc70f895edf75277e53a23c5799994f1702
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-10-29 14:29:30 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="userUpload" id="userUpload">
+        <button type="submit" name="submit">submit</button>
+    </form>
+
+    <?php
+
+    ?>
+</body>
+
+</html>
